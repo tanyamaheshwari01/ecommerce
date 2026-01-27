a353408e2b97d9c83a05906e8f0ee4fef111cefd
tanyamaheshwari01 <tanyamaheshwari2912@gmail.com>
2026-01-27 12:59:59 +0530

    chaanges

--- types/product.ts
+++ types/product.ts
@@ -4,15 +4,14 @@ export interface Product {
   price: number;
   description: string;
   category: string;
-  thumbnail: string; // DummyJSON ki main image property
-  images: string[];  // Saari images ka array
-  rating: number;    // DummyJSON mein ye sirf ek number hai (e.g. 4.69)
-  stock: number;     // Extra info jo DummyJSON deta hai
-  brand?: string;    // Optional, assignment mein kaam aa sakta hai
+  thumbnail: string; 
+  images: string[];  
+  rating: number;    
+  stock: number;     
+  brand?: string;    
   discountPercentage?: number; 
 }
 
-// API Response ke liye bhi type bana lo taaki 'any' ka error na aaye
 export interface DummyJSONResponse {
   products: Product[];
   total: number;
